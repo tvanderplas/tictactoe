d801800cc3464323f1e1b836d7facabd69fc025c
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-08-05 12:49:09 -0400

    add menu

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -1,6 +1,22 @@
 
 def main():
-    pass
+    Game()
+
+
+class Game():
+
+    def __init__(self):
+        self.menu()
+
+
+    def menu(self):
+        print('1) Human vs. Human')
+        print('2) Human vs. Computer')
+        mode = input('Select mode:')
+        while mode not in ['1', '2']:
+            print('Please select 1 or 2')
+            mode = input('Select mode')
+        return int(mode)
 
 
 if __name__ == '__main__':
