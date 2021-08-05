b8265506bf1104495eff09873adaad0a07ecce22
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-08-05 12:58:09 -0400

    fix prompt

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -10,12 +10,13 @@ class Game():
 
 
     def menu(self):
+        prompt = 'Select mode:'
         print('1) Human vs. Human')
         print('2) Human vs. Computer')
-        mode = input('Select mode:')
+        mode = input(prompt)
         while mode not in ['1', '2']:
             print('Please select 1 or 2')
-            mode = input('Select mode')
+            mode = input(prompt)
         return int(mode)
 
 
