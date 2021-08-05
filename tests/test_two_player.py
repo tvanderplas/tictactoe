23aecc69c42a265d21c89a0c579e47c95891d9b6
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-08-05 12:30:45 -0400

    add test stub

---
+++ tests/test_two_player.py
@@ -0,0 +1,13 @@
+
+import context
+import tictactoe # pylint: disable=import-error
+import unittest
+
+class TwoPlayerTest(unittest.TestCase):
+
+    def test_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
