7f8b94738a7da1d90b32c69a9f7970f1a056ef4e
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-08-05 16:41:54 -0400

    add tests

--- tests/test_two_player.py
+++ tests/test_two_player.py
@@ -76,5 +76,44 @@ class TwoPlayerTest(unittest.TestCase):
         self.assertEqual(setup.view, expected)
 
 
+    def test_x_wins_with_3_in_a_row(self):
+        setup = tictactoe.Tictactoe()
+        setup.mark(5)
+        setup.mark(4)
+        setup.mark(7)
+        setup.mark(1)
+        setup.mark(3)
+        expected = '\n'.join([
+            ' X |   |   ',
+            '---+---+---',
+            ' O | X |   ',
+            '---+---+---',
+            ' O |   | X ',
+        ])
+        self.assertEqual(setup.view, expected)
+        self.assertEqual(setup.is_complete, True)
+        self.assertEqual(setup.winner, 'X')
+
+
+    def test_o_wins_with_3_in_a_row(self):
+        setup = tictactoe.Tictactoe()
+        setup.mark(7)
+        setup.mark(8)
+        setup.mark(4)
+        setup.mark(5)
+        setup.mark(3)
+        setup.mark(2)
+        expected = '\n'.join([
+            ' X | O |   ',
+            '---+---+---',
+            ' X | O |   ',
+            '---+---+---',
+            '   | O | X ',
+        ])
+        self.assertEqual(setup.view, expected)
+        self.assertEqual(setup.is_complete, True)
+        self.assertEqual(setup.winner, 'O')
+
+
 if __name__ == '__main__':
     unittest.main()
