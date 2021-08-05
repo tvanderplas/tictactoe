beea6484d539ec96e83b4674870cb5aa445cd423
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2021-08-05 14:59:19 -0400

    change test

--- tests/test_two_player.py
+++ tests/test_two_player.py
@@ -6,14 +6,14 @@ import unittest
 class TwoPlayerTest(unittest.TestCase):
 
     def test_starting_game_gets_empty_board(self):
-        empty_board = '\n'.join([
+        expected = '\n'.join([
             '   |   |   ',
             '---+---+---',
             '   |   |   ',
             '---+---+---',
             '   |   |   ',
         ])
-        self.assertEqual(tictactoe.Tictactoe().view, empty_board)
+        self.assertEqual(tictactoe.Tictactoe().view, expected)
 
 
 if __name__ == '__main__':
